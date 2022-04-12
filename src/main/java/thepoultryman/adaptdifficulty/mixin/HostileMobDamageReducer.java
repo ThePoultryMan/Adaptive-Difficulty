53339c52cadab83b647d211f5e9363c847b1017d
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-11 23:25:10 -0400

    hehe.0f

--- src/main/java/thepoultryman/adaptdifficulty/mixin/HostileMobDamageReducer.java
+++ src/main/java/thepoultryman/adaptdifficulty/mixin/HostileMobDamageReducer.java
@@ -7,15 +7,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import thepoultryman.adaptdifficulty.AdaptiveDifficulty;
 
 @Mixin(LivingEntity.class)
 public class HostileMobDamageReducer {
 	@Inject(at = @At("HEAD"), method = "applyDamage")
 	private void adaptdifficulty$reducePlayerDamage(DamageSource source, float amount, CallbackInfo ci) {
 		if (source.getAttacker() instanceof ServerPlayerEntity player) {
-			amount = (float) (amount - (Math.pow(1.1, (player.experienceLevel - 30)) + 1));
-			AdaptiveDifficulty.LOGGER.info(String.valueOf(amount));
+			amount = (float) (amount - (Math.pow(1.1f, (player.experienceLevel - 30)) + 1));
 		}
 	}
 }
